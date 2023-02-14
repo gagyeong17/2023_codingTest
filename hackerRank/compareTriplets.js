20ffba566cf2a306ce219131bf5a5f7cf1b66387
GYMMX <rkrud951117@naver.com>
2023-02-14 18:04:04 +0900

    hackerRank Compare Triplets 문제풀이, https://www.hackerrank.com/challenges/compare-the-triplets/problem?isFullScreen=true

---
+++ hackerRank/compareTriplets.js
@@ -0,0 +1,13 @@
+function compareTriplets(a, b) {
+  let Alice = 0;
+  let Bob = 0;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] > b[i]) {
+      Alice++;
+    } else if (a[i] < b[i]) {
+      Bob++;
+    }
+  }
+  console.log(Alice);
+  console.log(Bob);
+}
