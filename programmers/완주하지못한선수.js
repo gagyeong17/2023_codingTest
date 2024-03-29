99c6c7deb496a836ff2bb24cfb8a885a2cf40d5a
kimgagyeong <rkrud951117@naver.com>
2024-03-29 18:11:34 +0900

    programmers, 완주하지못한선수, https://school.programmers.co.kr/learn/courses/30/lessons/42576
    해시테이블로 다시 풀기

--- programmers/완주하지못한선수.js
+++ programmers/완주하지못한선수.js
@@ -1,11 +1,30 @@
-function solution(participant, completion) {
-  //이름을 알파벳 순으로 정렬하고
-  participant.sort();
-  completion.sort();
-  for (let i = 0; i < participant.length; i++) {
-    //i번째 순서의 참가자가 같지 않을 경우 return
-    if (participant[i] !== completion[i]) {
-      return participant[i];
+function solution(participants, completions) {
+  // hash table
+  // Map으로 풀기
+  let answer = "";
+  let myMap = new Map();
+
+  //참여자 목록을 Map에 등록하기
+  for (const participant of participants) {
+    // 참여자가 없다면 참여자를 => 1로 등록
+    if (!myMap.has(participant)) {
+      myMap.set(participant, 1);
+    } else {
+      // 참여자가 있는데 그 이름이 또 있다면?(동명이인) 기존 1에 + 1해주기 => 2로 등록
+      myMap.set(participant, myMap.get(participant) + 1);
     }
   }
+
+  // 완주자 목록을 myMap에서 찾아서 -1 해주기
+  for (const completion of completions) {
+    myMap.set(completion, myMap.get(completion) - 1);
+  }
+
+  // myMap에서 value가 1 이상 인 key를 찾아서 반환하기
+  for (const [key, value] of myMap) {
+    if (value > 0) {
+      answer = key;
+    }
+  }
+  return answer;
 }
